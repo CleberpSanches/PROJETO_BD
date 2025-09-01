6223ac3575d154fea4a84a28a7b25d92d958f246
Cleberpsanches <jscleberpsanches@gmail.com>
2025-09-01 10:44:52 -0300

    configs 2

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("oi");
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: ""//colocar o nome depois
+  });

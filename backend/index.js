0a2259e1632c64fd3f5891bf561483866f3161ce
CleberpSanches <Jscleberpsanches@gmail.com>
2025-09-24 19:23:12 +0000

    teste modularização

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ app.use(express.json());
 // Rotas
 const clientesRoutes = require('./routes/clientes');
 app.use('/clientes', clientesRoutes);
-
+const lancamentosRoutes = require('./routes/financeiro/lancamentos');
+app.use('/lancamentos', lancamentosRoutes);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
---
+++ backend/routes/financeiro/lancamentos.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../bd/bd');
+
+// GET todos os clientes
+router.get('/', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT * FROM lancamentos_inanceiros');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao buscar lancamentos' });
+  }
+});
+
+// POST criar um lancamento
+router.post('/', async (req, res) => {
+  const {pagamento_id, conta_financeira_id, tipo, valor, data_lancamento, conciliado, conta_receber_id, conta_pagar_id} = req.body;
+
+  try {
+    const [result] = await pool.query(
+      'INSERT INTO lancamentos_financeiros (pagamento_id, conta_financeira_id, tipo, valor, data_lancamento, conciliado, conta_receber_id, conta_pagar_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+      [pagamento_id, conta_financeira_id, tipo, valor, data_lancamento, conciliado, conta_receber_id, conta_pagar_id]
+    );
+    res.json({ id: result.insertId });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao criar lançamentos' });
+  }
+});
+
+module.exports = router;

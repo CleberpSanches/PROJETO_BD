1049aa9fb4ca734110eab94bc3aaf1ae5baab970
Eduarda <164544759+eduardapastore@users.noreply.github.com>
2025-10-27 19:03:37 +0000

    loginpage update

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Login = () => {
   return (
     <div className='flex min-h-screen bg-orange-50'>
@@ -8,27 +6,29 @@ const Login = () => {
         className="w-1/2 min-h-screen bg-cover bg-center bg-no-repeat relative"
         style={{ backgroundImage: "url('/src/imgs/signimg.png')" }}
       >
-        <div className="relative z-10 h-full flex justify-end p-5">
-          <a className='text-white text-sm font-regular text-right px-8 border-b border-white'>
-            Sou Cliente
-          </a>
-          <a className='text-white text-sm font-regular text-right px-8'>
-            Sou Colaborador
-          </a>
+        <div className="relative z-10 flex gap-4 p-5 align-baseline justify-end">
+          <button className='p-2 text-white text-xs font-regular px-8 border rounded-full border-white hover:bg-orange-700 hover:border-none'>
+            <a href='#'>
+              Sou Cliente
+            </a>
+          </button>
+          <button className='p-2 bg-orange-600 font-bold text-white text-xs font-regular px-8 rounded-full border-white'>
+              Sou Colaborador
+          </button>
         </div>
       </div>
 
       {/* Lado direito - Formulário de login */}
       <div className='p-5 w-1/2 justify-center items-center flex flex-col gap-6'>
         <img src='/src/imgs/Glamsync.png' width="256px"></img>
-        <input type='name' placeholder='Usuário' className='p-4 rounded-sm border bg-transparent'/>
-        <input type='password' placeholder='Senha' className='p-4 rounded-sm border bg-transparent' />
-        <a className='font-bold text-orange-700 text-sm text-right' >Esqueceu a senha?</a>
-        <button className='bg-orange-700 hover:bg-orange-900 text-white font-bold py-2 px-4 rounded-full mt-4'>
+        <input type='name' placeholder='Usuário' className='p-3 rounded-md border border-black bg-transparent'/>
+        <input type='password' placeholder='Senha' className='p-3 rounded-md border border-black bg-transparent' />
+        <button className='bg-orange-600 hover:bg-orange-900 text-white font-bold py-2 px-4 rounded-lg'>
           <a>
-            Enta
+            Entrar
           </a>
         </button>
+        <a href='#' className='font-semibold text-orange-600 text-xs text-right underline' >Esqueceu a senha?</a>
       </div>
     </div>
   )
